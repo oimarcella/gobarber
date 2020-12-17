09649eb44143562d8efd98037e6655e7e2d4143a
Marcella LASA <marcella.amorimsa@gmail.com>
2020-12-17 13:08:11 -0300

    starting with unform

--- gobarber-web/src/pages/SignUp/index.tsx
+++ gobarber-web/src/pages/SignUp/index.tsx
@@ -1,7 +1,7 @@
-import React, { useState, FormEvent } from 'react';
+import React from 'react';
+import { Link } from 'react-router-dom';
 import { FiMail, FiLock, FiUser, FiArrowLeft } from 'react-icons/fi';
-
-import api from '../../services/api';
+import { Form } from '@unform/web';
 
 import logoImg from '../../assets/logo.svg';
 
@@ -11,15 +11,8 @@ import Input from '../../components/Input/index';
 import { Container, Background, Content } from './styles';
 
 const SignUp: React.FC = () => {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-
-  const handleLogin = (event: FormEvent<HTMLFormElement>): void => {
-    event.preventDefault();
-
-    api.post('/sessions/', { email, password }).then(response => {
-      localStorage.setItem('@GoBarber:login', JSON.stringify(response.data));
-    });
+  const handleSubmit = (data: object): void => {
+    console.log('This is handle handleSubmit function', data);
   };
 
   return (
@@ -28,34 +21,24 @@ const SignUp: React.FC = () => {
         <Background />
         <Content>
           <img src={logoImg} alt="GoBarber logo" />
-          <form onSubmit={handleLogin}>
+
+          <Form initialData={{ name: 'Marcella' }} onSubmit={handleSubmit}>
             <h1>Faça seu cadastro</h1>
 
-            <Input
-              name="name"
-              icon={FiUser}
-              placeholder="Nome"
-              onChange={e => setEmail(e.target.value)}
-            />
-            <Input
-              name="email"
-              icon={FiMail}
-              placeholder="E-mail"
-              onChange={e => setEmail(e.target.value)}
-            />
+            <Input name="name" icon={FiUser} placeholder="Nome" />
+            <Input name="email" icon={FiMail} placeholder="E-mail" />
             <Input
               name="password"
               icon={FiLock}
               type="password"
               placeholder="Senha"
-              onChange={e => setPassword(e.target.value)}
             />
             <Button type="submit">Cadastrar</Button>
-          </form>
-          <a href="home">
+          </Form>
+          <Link to="/">
             <FiArrowLeft />
             Voltar para logon
-          </a>
+          </Link>
         </Content>
       </Container>
     </>
--- gobarber-web/src/styles/global.ts
+++ gobarber-web/src/styles/global.ts
@@ -20,9 +20,6 @@ export default createGlobalStyle`
   h1,h2,h3,h4,h5,h6,strong{
     font-weight:500;
   }
-  a{
-    text-decoration:none;
-  }
   button{
     cursor:pointer;
   }
