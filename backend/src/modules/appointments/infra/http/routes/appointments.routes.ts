a4f5afca7e53c300da6ccf766d92a735a720d902
Marcella LASA <marcella.amorimsa@gmail.com>
2021-04-07 19:42:03 -0300

    refactor: eslintrc

--- backend/src/modules/appointments/infra/http/routes/appointments.routes.ts
+++ backend/src/modules/appointments/infra/http/routes/appointments.routes.ts
@@ -1,6 +1,5 @@
 import { Router } from 'express';
 import { parseISO } from 'date-fns';
-import { getCustomRepository } from 'typeorm';
 
 import AppointmentsRepository from '@modules/appointments/infra/typeorm/repositories/AppointmentsRepository';
 import CreateAppointmentService from '@modules/appointments/services/CreateAppointmentService';
@@ -8,28 +7,28 @@ import CreateAppointmentService from '@modules/appointments/services/CreateAppoi
 import ensureAuthenticated from '@modules/users/infra/http/middlewares/ensureAuthenticated';
 
 const appointmentsRouter = Router();
+const appointmentsRepository = new AppointmentsRepository();
 
 appointmentsRouter.use(ensureAuthenticated);
+
+/* appointmentsRouter.get('/', async (request, response) => {
+	const appointments = await appointmentsRepository.find();
+
+	return response.status(200).json(appointments);
+}); */
 
 appointmentsRouter.post('/', async (request, response) => {
 	const { provider_id, date } = request.body;
 
 	const dateParsed = parseISO(date);
 
-	const createAppointmentService = new CreateAppointmentService();
+	const createAppointmentService = new CreateAppointmentService(appointmentsRepository);
 	const appointment = await createAppointmentService.execute({
 		provider_id,
 		date: dateParsed,
 	});
 
 	return response.status(200).json(appointment);
-});
-
-appointmentsRouter.get('/', async (request, response) => {
-	const appointmentsRepository = getCustomRepository(AppointmentsRepository);
-	const appointments = await appointmentsRepository.find();
-
-	return response.status(200).json(appointments);
 });
 
 export default appointmentsRouter;
